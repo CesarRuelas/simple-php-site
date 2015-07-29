cbf96efc3ba8a05cd38c1b4b34c02f27e2c0eb39
CesarRuelas <cesarruelas.bm@gmail.com>
2015-07-29 17:37:12 -0500

    Built templates with Twig and linked them correspondingly

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 require __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set('America/Monterrey');
 
@@ -9,14 +10,26 @@ use Monolog\Handler\StreamHandler;
 // $log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
 // $log->addWarning('Oh no');
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+	'view' => new \Slim\Views\Twig()
+));
+
+$view = $app->view();
+
+$view->parserOptions = array(
+    'debug' => true
+);
+
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
 
 $app->get('/', function () use($app) {
-    $app->render('index.html');
+    $app->render('about.twig');
 });
 
 $app->get('/contact', function () use ($app) {
-    $app->render('contact.html');
+    $app->render('contact.twig');
 });
 
 $app->run();
